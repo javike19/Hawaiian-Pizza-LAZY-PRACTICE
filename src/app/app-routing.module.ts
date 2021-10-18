b1e050f2ec06204ec5b99274b733400b5213900d
unknown <javimorenomontejo@gmail.com>
2021-10-18 19:37:34 +0200

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ``, loadChildren: () =>
+      import('./@pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: `gallery`, loadChildren: () =>
+      import('./@pages/gallery/gallery.module').then(m => m.GalleryModule)
+  },
+  {
+    path: `contact`, loadChildren: () =>
+      import('./@pages/contact/contact.module').then(m => m.ContactModule)
+  },
+  { path: ``, redirectTo: ``, pathMatch: `full`}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
